8a0274caf0db3da5b02426e2fdbde532a568fbb3
nuryudha <nur.yudha16@gmail.com>
2023-08-10 07:10:47 +0700

    first

---
+++ src/main/java/com/domain/models/entities/Product.java
@@ -0,0 +1,38 @@
+package com.domain.models.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tbl_product")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_name", length = 100)
+    private String name;
+
+    @Column(name = "product_description", length = 500)
+    private String description;
+
+    private double price;
+
+}
